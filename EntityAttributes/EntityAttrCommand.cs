4ccd7327c5ea4d0f25ea081054442557c599521f
Liviu <Liviu@DESKTOP-LIVIU>
2025-04-18 18:10:01 +0300

    Add project files.

---
+++ EntityAttributes/EntityAttrCommand.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+
+namespace EntityAttributes
+{
+    internal class EntityAttrCommand : IChatCommand
+    {
+        private List<string> aliases;
+
+        public IChatCommand this[string name] => throw new NotImplementedException();
+
+        public string FullName => "entityattributes";
+
+        public string Name => "eattr";
+
+        public string Description => "Manage the attributes of an entity";
+
+        public string AdditionalInformation => "This command allows you to get all attributes that an entity has, such as current health, max health, weight and so on, modify an existing attribute or adding new ones";
+
+        public string[] Examples => new string[] {
+            "/eattr <target> get",
+            "/eattr <target> set <attrName> <dataType> <value>"
+        };
+
+        public bool Incomplete => false;
+
+        public List<string> Aliases
+        {
+            get {
+                List<string> aliases = new();
+                aliases.AddRange(new string[] { "entityattr" });
+                return aliases;
+            }
+        }
+
+        public List<string> RootAliases => throw new NotImplementedException();
+
+        public string CommandPrefix => throw new NotImplementedException();
+
+        public IEnumerable<IChatCommand> Subcommands => throw new NotImplementedException();
+
+        public Dictionary<string, IChatCommand> AllSubcommands => throw new NotImplementedException();
+
+        public string CallSyntax => throw new NotImplementedException();
+
+        public string CallSyntaxUnformatted => throw new NotImplementedException();
+
+        public void AddParameterSyntax(StringBuilder sb, string indent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSyntaxExplanation(StringBuilder sb, string indent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand BeginSubCommand(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand BeginSubCommands(params string[] name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand EndSubCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(TextCommandCallingArgs callargs, Action<TextCommandResult> onCommandComplete = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCallSyntax(string alias, bool isRootAlias = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCallSyntaxUnformatted(string alias, bool isRootAlias = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFullName(string alias, bool isRootAlias = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFullSyntaxConsole(Caller caller)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFullSyntaxHandbook(Caller caller, string indent = "", bool isRootAlias = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand HandleWith(OnCommandDelegate handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand IgnoreAdditionalArgs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAvailableTo(Caller caller)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand RequiresPlayer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand RequiresPrivilege(string privilege)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand WithAdditionalInformation(string detail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand WithAlias(params string[] name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand WithArgs(params ICommandArgumentParser[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand WithDescription(string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand WithExamples(params string[] examaples)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand WithName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand WithPreCondition(CommandPreconditionDelegate p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChatCommand WithRootAlias(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EntityAttributes/EntityAttributesModSystem.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.CommandAbbr;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Config;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.Server;
+using Vintagestory.API.Util;
+
+namespace EntityAttributes
+{
+    public class EntityAttributesModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            Lang.Load(api.Logger, api.Assets);
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.ChatCommands.Create("entityattributes")
+                .WithDescription(Lang.Get("eattr-cmd-desc"))
+                .WithAdditionalInformation(Lang.Get("eattr-cmd-additional"))
+                .WithAlias(new string[] { "entityattr", "eattr" })
+                .WithRootAlias("eattr")
+                .WithExamples(new string[]
+                {
+                    "/eattr <target> get",
+                    "/eattr <target> set <attrName> <value>",
+                    "/eattr <target> revive"
+                })
+                .RequiresPrivilege(Privilege.chat)
+                .RequiresPlayer()
+                .WithArgs(api.ChatCommands.Parsers.Entities("target"))
+                .IgnoreAdditionalArgs()
+                .HandleWith((args) =>
+                {
+                    IPlayer player = args.Caller.Player;
+                    var parser = args.Parsers.Find((parser) => parser.ArgumentName == "target").GetValue();
+
+                    if (args.ArgCount > 0)
+                    {
+                        Entity[] targets = (Entity[]) parser;
+
+                        if(targets.Length > 1)
+                        {
+                            return TextCommandResult.Error("Only one entity can be selected at a time!");
+                        }
+
+                        if(args.ArgCount > 1)
+                        {
+                            switch(args[1])
+                            {
+                                case "revive":
+                                    foreach (Entity en in targets)
+                                    {
+                                        float maxHealth = en.WatchedAttributes.GetTreeAttribute("health").GetFloat("maxhealth");
+
+                                        if (en.WatchedAttributes.TryGetFloat("mortallyWoundedTotalHours") != null)
+                                        {
+                                            en.WatchedAttributes.RemoveAttribute("mortallyWoundedTotalHours");
+                                        }
+
+                                        if(en.WatchedAttributes.TryGetInt("healthState") != null)
+                                        {
+                                            en.WatchedAttributes.SetInt("healthState", 1);
+                                        }
+
+                                        if (en.WatchedAttributes.GetTreeAttribute("health").GetFloat("currenthealth") < maxHealth)
+                                        {
+                                            en.WatchedAttributes.GetTreeAttribute("health").SetFloat("maxhealth", maxHealth);
+                                        }
+
+                                        api.SendMessage(player, 0, "Entity '" + en.EntityId + "' has been revived", EnumChatType.Notification);
+                                    }
+
+                                    return TextCommandResult.Success();
+                                default:
+                                    return TextCommandResult.Error("Unknown argument! Type '/help eattr' for examples");
+                            }
+                        } else
+                        {
+                            return TextCommandResult.Error("Invalid syntax! Type '/help eattr' for examples");
+                        }
+                    } else
+                    {
+                        return TextCommandResult.Error("Too few arguments");
+                    }
+                });
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            
+        }
+
+    }
+}
