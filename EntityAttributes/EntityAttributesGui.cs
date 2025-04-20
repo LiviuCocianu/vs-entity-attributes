a1002b75fdd1a217f61592f229c044a3b4d2f9f2
LiviuCocianu <liviucocianu@gmail.com>
2025-04-20 19:35:54 +0300

    - Fixed "revive" subcommand to work on regular entities too instead of just dogs from the Wolf Taming mod - Prepared for transitioning command to GUI

---
+++ EntityAttributes/EntityAttributesGui.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common.Entities;
+
+namespace EntityAttributes {
+    public class EntityAttributesGui : GuiDialog {
+        public override string ToggleKeyCombinationCode => "eattrgui";
+        private Entity target;
+
+        private Entity Target {
+            get { return null; }
+            set { target = value; }
+        }
+
+        public EntityAttributesGui(ICoreClientAPI capi, Entity en) : base(capi) {
+            Target = en;
+            SetupDialog();
+        }
+
+        private void SetupDialog() {
+
+        }
+
+        private void OnTitleBarCloseClicked() {
+            TryClose();
+        }
+    }
+}
--- EntityAttributes/EntityAttributesModSystem.cs
+++ EntityAttributes/EntityAttributesModSystem.cs
@@ -7,6 +7,7 @@ using Vintagestory.API.Config;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.Server;
 using Vintagestory.API.Util;
+using Vintagestory.Client.NoObf;
 
 namespace EntityAttributes
 {
@@ -17,80 +18,62 @@ namespace EntityAttributes
         // Useful for registering block/entity classes on both sides
         public override void Start(ICoreAPI api)
         {
-            Lang.Load(api.Logger, api.Assets);
+
         }
 
         public override void StartServerSide(ICoreServerAPI api)
         {
             api.ChatCommands.Create("entityattributes")
-                .WithDescription(Lang.Get("eattr-cmd-desc"))
-                .WithAdditionalInformation(Lang.Get("eattr-cmd-additional"))
+                .WithDescription(Lang.Get("entityattributes:cmd-desc"))
+                .WithAdditionalInformation(Lang.Get("entityattributes:cmd-additional"))
                 .WithAlias(new string[] { "entityattr", "eattr" })
                 .WithRootAlias("eattr")
                 .WithExamples(new string[]
                 {
                     "/eattr <target> get",
-                    "/eattr <target> set <attrName> <value>",
                     "/eattr <target> revive"
                 })
                 .RequiresPrivilege(Privilege.chat)
                 .RequiresPlayer()
-                .WithArgs(api.ChatCommands.Parsers.Entities("target"))
-                .IgnoreAdditionalArgs()
-                .HandleWith((args) =>
-                {
+                .WithArgs(new EntitiesArgParser("target", api, true))
+                .BeginSub("revive")
+                .WithDesc(Lang.Get("entityattributes:cmd-revive-desc"))
+                .WithAdditionalInformation(Lang.Get("entityattributes:cmd-revive-additional"))
+                .HandleWith((args) => {
                     IPlayer player = args.Caller.Player;
                     var parser = args.Parsers.Find((parser) => parser.ArgumentName == "target").GetValue();
+                    Entity[] targets = (Entity[])parser;
 
-                    if (args.ArgCount > 0)
-                    {
-                        Entity[] targets = (Entity[]) parser;
+                    foreach (Entity en in targets) {
+                        string name = en.GetName() == "" ? (en.CodeWithoutParts(1) ?? "Unknown") : en.GetName();
+                        float maxHealth = en.WatchedAttributes.GetTreeAttribute("health").GetFloat("maxhealth");
 
-                        if(targets.Length > 1)
-                        {
-                            return TextCommandResult.Error("Only one entity can be selected at a time!");
-                        }
-
-                        if(args.ArgCount > 1)
-                        {
-                            switch(args[1])
-                            {
-                                case "revive":
-                                    foreach (Entity en in targets)
-                                    {
-                                        float maxHealth = en.WatchedAttributes.GetTreeAttribute("health").GetFloat("maxhealth");
-
-                                        if (en.WatchedAttributes.TryGetFloat("mortallyWoundedTotalHours") != null)
-                                        {
-                                            en.WatchedAttributes.RemoveAttribute("mortallyWoundedTotalHours");
-                                        }
-
-                                        if(en.WatchedAttributes.TryGetInt("healthState") != null)
-                                        {
-                                            en.WatchedAttributes.SetInt("healthState", 1);
-                                        }
-
-                                        if (en.WatchedAttributes.GetTreeAttribute("health").GetFloat("currenthealth") < maxHealth)
-                                        {
-                                            en.WatchedAttributes.GetTreeAttribute("health").SetFloat("maxhealth", maxHealth);
-                                        }
-
-                                        api.SendMessage(player, 0, "Entity '" + en.EntityId + "' has been revived", EnumChatType.Notification);
-                                    }
-
-                                    return TextCommandResult.Success();
-                                default:
-                                    return TextCommandResult.Error("Unknown argument! Type '/help eattr' for examples");
+                        if(en.AnimManager.IsAnimationActive("wounded-idle")) {
+                            if (en.WatchedAttributes.TryGetFloat("mortallyWoundedTotalHours") != null) {
+                                en.WatchedAttributes.RemoveAttribute("mortallyWoundedTotalHours");
                             }
-                        } else
-                        {
-                            return TextCommandResult.Error("Invalid syntax! Type '/help eattr' for examples");
+
+                            if (en.WatchedAttributes.TryGetInt("healthState") != null) {
+                                en.WatchedAttributes.SetInt("healthState", 0);
+                            }
+
+                            if (en.WatchedAttributes.GetTreeAttribute("health").GetFloat("currenthealth") < maxHealth) {
+                                en.WatchedAttributes.GetTreeAttribute("health").SetFloat("currenthealth", maxHealth);
+                            }
+
+                            en.AnimManager.StopAnimation("wounded-idle");
+                        } else {
+                            en.State = EnumEntityState.Active;
+                            en.Revive();
                         }
-                    } else
-                    {
-                        return TextCommandResult.Error("Too few arguments");
+
+                        api.SendMessage(player, 0, "Entity '" + name + "' (" + en.EntityId + ") has been revived", EnumChatType.Notification);
                     }
-                });
+
+                    return TextCommandResult.Success();
+                })
+                .EndSub()
+                .IgnoreAdditionalArgs();
         }
 
         public override void StartClientSide(ICoreClientAPI api)
